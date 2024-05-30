04262016588380306ea2f744f20e04d571c95be6
GALLERIA <kaitomx@gmail.com>
2024-05-30 23:56:34 +0900

    add HorseDataStruct

---
+++ HorseData/HorseDataStruct.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 yumekasa5
+import numpy as np
+
+class HorseDataStruct:
+    def __init__(self):
+        self.horseName = ""
+        self.horseID = ""
+        self.horseSex = ""
+        self.horseAge = 0
+        self.horseOwner = ""
+        self.horseTrainer = ""
+        self.horseJockey = ""
+        self.horseWeight = 0
+        self.horseWeightDiff = 0
+        self.horseWeightDiffSign = ""
+        self.horseWeightCarried = 0
+        self.horseWeightCarriedDiff = 0
